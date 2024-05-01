538478d16bd6762cb904612e90519c89d97a608c
Sheikh Bahauddin Arnab <arnab.sheikh007@gmail.com>
2024-05-01 12:36:28 +0600

    Added User Dashboard

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Employee;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return view('user.dashboard');
+        $userId = Auth::id();
+        $employee = Employee::where('user_id', $userId)->first();
+        $user = User::findOrFail($userId);
+        $leaveRequests = null;
+        if ($user->employee) {
+            $leaveRequests = $user->employee->leaveRequests()->paginate(5);
+        }
+        return view('user.dashboard', compact('employee', 'employee', 'leaveRequests', 'leaveRequests'));
     }
 }
--- resources/views/User/dashboard.blade.php
+++ resources/views/User/dashboard.blade.php
@@ -4,14 +4,123 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+    @if($employee == null || $employee->status !== 'active')
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        {{ __("You're not listed as an employee!!!") }}
+                    </div>
+                    <div class="p-6 text-gray-900">
+                        {{ __("Please contact with admin") }}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @else
+        <div class="py-10">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <a href="#" class="block p-6 text-gray-900 text-center hover:underline">
+                        {{ __("Create a leave request") }}
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="pb-10">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6">
+                        <table class="w-full table-auto">
+                            <thead>
+                            <tr>
+                                <th class="px-4 py-2">Casual Leave</th>
+                                <th class="px-4 py-2">Sick Leave</th>
+                                <th class="px-4 py-2">Emergency Leave</th>
+                                <th class="px-4 py-2">Total Leave</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td class="px-4 py-2 text-center">{{ $employee->casual_leave }}</td>
+                                <td class="px-4 py-2 text-center">{{ $employee->sick_leave }}</td>
+                                <td class="px-4 py-2 text-center">{{ $employee->emergency_leave }}</td>
+                                <td class="px-4 py-2 text-center">{{ $employee->total_leave }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="py-10">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <div>
+                            <h2 class="text-center font-semibold text-xl text-gray-800 leading-tight">
+                                {{ __('Leave Requests') }}
+                            </h2>
+                        </div>
+
+                        <div class="bg-white p-4 rounded-lg">
+                            <div class="overflow-x-auto">
+                                <table class="w-full table-auto">
+                                    <thead>
+                                    <tr class="bg-gray-200">
+                                        <th class="px-4 py-2 text-center">#Sl No</th>
+                                        <th class="px-4 py-2 text-start">Leave Type</th>
+                                        <th class="px-4 py-2 text-start">From</th>
+                                        <th class="px-4 py-2 text-start">To</th>
+                                        <th class="px-4 py-2 text-start">Reason</th>
+                                        <th class="px-4 py-2 text-center">Status</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @if($leaveRequests == null || $leaveRequests->isEmpty())
+                                        <tr>
+                                            <td colspan="4" class="text-center">No Leave Request</td>
+                                        </tr>
+                                    @endif
+                                    @foreach($leaveRequests as $leaveRequest)
+                                        <tr class="hover:bg-gray-100">
+                                            <td class="px-4 py-2 text-center">{{ ($leaveRequests->currentPage()-1) * $leaveRequests->perPage() + $loop->iteration }}</td>
+                                            <td class="px-4 py-2">{{ $leaveRequest->type }}</td>
+                                            <td class="px-4 py-2">{{ $leaveRequest->from }}</td>
+                                            <td class="px-4 py-2">{{ $leaveRequest->to }}</td>
+                                            <td class="px-4 py-2">{{ $leaveRequest->duration }}</td>
+                                            <td class="px-4 py-2 text-center">
+                                                <a href="" style="background-color:#68D391" class="text-white font-bold py-2 px-4 rounded">
+                                                    {{ $leaveRequest->status }}
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="mt-4">
+                                {{ $leaveRequests->onEachSide(1)->links() }}
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+{{--        <div class="py-10">--}}
+{{--            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">--}}
+{{--                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">--}}
+{{--                    <div class="p-6 text-gray-900">--}}
+{{--                        {{ __("You're logged in!") }}--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+    @endif
+
 </x-app-layout>
